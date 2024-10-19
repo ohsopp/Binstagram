6747eb478816555f13e7569eadc1fd4ea87c8b78
ohsopp <wjdgus0545@naver.com>
2024-10-19 15:46:35 +0900

    feat: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink> | 
+      <RouterLink to="/about">About</RouterLink> |
+      <RouterLink to="/qwer">QWER</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/views/QwerView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>QWER</h1>
+</template>
